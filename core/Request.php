34bc96ee9abde06a1e88e31f6e0d526827462e6c
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-10-10 20:11:47 +0700

    ``build(Composer)``: Implement basic routing

--- core/Request.php
+++ core/Request.php
@@ -3,14 +3,18 @@ namespace Saphpi;
 
 class Request {
     public function getPath(): string {
-        $path = $_SERVER['REQUEST_URI'];
-        
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+
         $queryStringPos = strpos($path, '?');
         if ($queryStringPos === false) {
             return $path;
         }
 
-        $path = substr($path, $queryStringPos);
+        $path = substr($path, 0, $queryStringPos);
         return $path;
+    }
+
+    public function getMethod(): string {
+        return strtolower($_SERVER['REQUEST_METHOD']);
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -2,19 +2,26 @@
 namespace Saphpi;
 
 class Router {
-    private array $route;
+    private array $routes;
     private Request $request;
 
     public function __construct(Request $request) {
         $this->request = $request;
     }
 
-    public function get(string $path, $callback): void {
-        $this->route['get'][$path] = $callback;
+    public function get(string $path, callable $callback): void {
+        $this->routes['get'][$path] = $callback;
     }
 
     public function resolve(): void {
-        $uri = $this->request->getPath();
-        echo "<pre>{$uri}</pre>";
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            echo '<h1>404 Not Found</h1>';
+            exit;
+        }
+
+        echo call_user_func($callback);
     }
 }
