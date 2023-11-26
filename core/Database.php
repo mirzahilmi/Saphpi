37a4deb4ee2450f7931c2c5828f8ab42881d8b62
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2023-11-27 05:22:36 +0700

    ``refactor(Database)``: Contain only basic method to start db connection

--- core/Database.php
+++ core/Database.php
@@ -2,60 +2,15 @@
 namespace Saphpi\Core;
 
 class Database {
-    private ?\PDO $connection;
+    public static ?\PDO $db;
 
-    public const MYSQL = 'mysql:host=%s;port=%d;dbname=%s';
-    public const POSTGRESQL = 'pgsql:host=%s;port=%d;dbname=%s';
+    private function __construct() {}
 
-    private readonly string $driver;
-    private readonly string $host;
-    private readonly string $port;
-    private readonly string $username;
-    private readonly string $password;
-    private readonly string $databaseName;
-
-    public function __construct(
-        string $driver,
-        string $host,
-        string $port,
+    public static function connect(
+        string $dsn,
         string $username,
         string $password,
-        string $databaseName
     ) {
-        $this->driver = $driver;
-        $this->host = $host;
-        $this->port = $port;
-        $this->username = $username;
-        $this->password = $password;
-        $this->databaseName = $databaseName;
-    }
-
-    public function establishConnection() : void {
-        try {
-            $this->connection = new \PDO(
-                sprintf(
-                    $this->driver,
-                    $this->host,
-                    $this->port,
-                    $this->databaseName
-                ),
-                $this->username,
-                $this->password
-            );
-            $this->connection->setAttribute(
-                \PDO::ATTR_ERRMODE,
-                \PDO::ERRMODE_EXCEPTION
-            );
-        } catch (\Throwable $th) {
-            die($th->getMessage());
-        }
-    }
-
-    public function closeConnection(): void {
-        $this->connection = null;
-    }
-
-    public function conn(): \PDO {
-        return $this->connection;
+        self::$db = new \PDO($dsn, $username, $password);
     }
 }
