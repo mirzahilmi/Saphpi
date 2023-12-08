20dd382e8653b91c9a049e696195b3d944730d26
mirza@Archturus <exquisitemirza@student.ub.ac.id>
2023-12-08 14:35:18 +0700

    ``feat``: Add Controller, Model & Rule to Create command

--- core/Console/Commands/Create.php
+++ core/Console/Commands/Create.php
@@ -10,8 +10,8 @@ class Create extends Command {
         $this->{$this->args[0]}($this->args[1]);
     }
 
-    private function create(string $name): void {
-        exec('touch ' . Application::$ROOT_DIR . "/{$name}");
+    private function create(string $name, string $content = ''): void {
+        file_put_contents(Application::$ROOT_DIR . "/{$name}", $content);
     }
 
     private function migration(string $name): void {
@@ -30,5 +30,32 @@ class Create extends Command {
         $id = sprintf('%04d', ++$numericIdentifier);
         $this->create("migrations/{$id}_create_{$name}_table.up.sql");
         $this->create("migrations/{$id}_create_{$name}_table.down.sql");
+    }
+
+    private function controller(string $name) {
+        $stubContent = file_get_contents(Application::$ROOT_DIR . '/core/Console/Commands/Stubs/Controller.stub');
+        if ($stubContent === false) {
+            throw new Exception('Controller stub file does not exist!');
+        }
+
+        $this->create("controllers/{$name}.php", sprintf($stubContent, $name));
+    }
+
+    private function model(string $name) {
+        $stubContent = file_get_contents(Application::$ROOT_DIR . '/core/Console/Commands/Stubs/Model.stub');
+        if ($stubContent === false) {
+            throw new Exception('Model stub file does not exist!');
+        }
+
+        $this->create("models/{$name}.php", sprintf($stubContent, $name));
+    }
+
+    private function rule(string $name) {
+        $stubContent = file_get_contents(Application::$ROOT_DIR . '/core/Console/Commands/Stubs/Rule.stub');
+        if ($stubContent === false) {
+            throw new Exception('Rule stub file does not exist!');
+        }
+
+        $this->create("rules/{$name}.php", sprintf($stubContent, $name));
     }
 }
